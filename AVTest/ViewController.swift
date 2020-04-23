1f30a967b066942a181db45afe2bcba53a46b746
Hugo Flores <hfp.ing2@gmail.com>
2020-04-23 12:35:56 -0400

    Controlls added

--- AVTest/ViewController.swift
+++ AVTest/ViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 import AVKit
 
 class ViewController: UIViewController {
+    
+    var soundEffect: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
     @IBAction func playVideo(_ sender: UIButton) {
         guard let url = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8") else {
             return
@@ -30,5 +33,34 @@ class ViewController: UIViewController {
             player.play()
         }
     }
+    
+    @IBAction func playLocalVideo(_ sender: Any) {
+        guard let path = Bundle.main.path(forResource: "myVideo", ofType:"mp4")
+        else {
+            print("Error loading video")
+            return
+        }
+        let url = URL(fileURLWithPath: path)
+        
+        let player = AVPlayer(url: url)
+        let controller = AVPlayerViewController()
+        controller.player = player
+        present(controller, animated: true) {
+            player.play()
+        }
+    }
+    
+    @IBAction func playLocalAudio(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "Hold The Line.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            soundEffect = try AVAudioPlayer(contentsOf: url)
+            soundEffect?.play()
+        } catch {
+            print("Error in loading mp3 from local")
+        }
+    }
+    
 }
 
